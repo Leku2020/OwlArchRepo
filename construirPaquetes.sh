e3d4947a6be73505a71a51b58d9c7c2ffc43124b
Leku2020 <lekujl@proton.me>
2024-12-27 09:56:22 +0100

    Added local compiling

---
+++ construirPaquetes.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+rm -R /owlArchRepo
+cp -R . /
+docker run --rm -v /owlArchRepo:/workspace:z archlinux:latest /bin/bash -c "
+	    echo 'Base:'
+	    ls -R .
+	    echo 'Workspace:'
+	    ls -l /workspace
+            useradd -m builder && echo 'builder:password' | chpasswd && \
+            pacman -Sy --noconfirm sudo base-devel && \
+            echo 'builder ALL=(ALL) NOPASSWD: /usr/bin/pacman' >> /etc/sudoers && \
+            chown -R builder:builder /workspace && \
+	    su - builder -c '
+              find /workspace -type f -name PKGBUILD | while read PKGBUILD; do
+                dir=\$(dirname \$PKGBUILD)
+                echo \"Building package in \$dir\"
+                cd \$dir && makepkg -si --noconfirm && cd -
+              done
+            '"
+
+
+mkdir -p "/owlArchRepo/pkgs/x86_64"
+find find /owlArchRepo -type f -name "*.pkg.tar.zst" | while read file; do
+	echo "Copying: $file to /owlArchRepo/pkgs/x86_64/"
+	cp "$file" "/owlArchRepo/pkgs/x86_64/"
+done
+
+docker run --rm -v /owlArchRepo:/workspace:z -w /workspace archlinux:latest /bin/bash -c '
+            pacman -Sy --noconfirm pacman-contrib && \
+            echo "Pacman actualizado" && \
+            
+            # Eliminar la base de datos existente si existe
+            rm -f /workspace/pkgs/x86_64/owlArchRepo.db.tar.gz && \
+            echo "Archivo de base de datos eliminado" && \
+            
+            # Crear la base de datos desde cero
+            repo-add /workspace/pkgs/x86_64/owlArchRepo.db.tar.gz && \
+            echo "Base de datos creada" && \
+            
+            # Añadir los paquetes válidos a la base de datos
+            cd /workspace/pkgs/x86_64 && \
+            echo "Cambiando a directorio /workspace/pkgs/x86_64" && \
+            
+            find . -type f -name "*.pkg.tar.zst" | while read pkg; do \
+              echo "Procesando: $pkg"; \
+              
+              # Verificar si el paquete es válido
+              if tar -I zstd -tf "$pkg" &> /dev/null; then \
+                echo "Paquete válido: $pkg"; \
+                repo-add /workspace/pkgs/x86_64/owlArchRepo.db.tar.gz "$pkg" && \
+                echo "Paquete añadido: $pkg"; \
+              else \
+                echo "Saltando paquete inválido: $pkg"; \
+              fi \
+            done'
